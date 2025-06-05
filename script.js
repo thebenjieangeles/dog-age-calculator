785be06f179086c545565f5c7f2c5b81fe342b11
thebenjieangeles <thebenjangeles@gmail.com>
2025-06-05 23:17:14 +0800

    corrected some of the wordings

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var whatDogAge = prompt("what is your dog's age?");
 var humanAge = (whatDogAge - 2) * 4 + 21;
-alert("Your dog's human age is " + humanAge);
+alert("Your dog's human age is " + humanAge + "! Yay!");
